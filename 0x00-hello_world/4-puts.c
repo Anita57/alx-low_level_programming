be4655576fc8453b497927fb17ce316cf3d54a16
Anita57 <anyaehieoluomachi@gmail.com>
2022-09-09 07:37:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,14 @@
- /* File: 4-puts.c
-  * Auth: Oluomachi A. Anyaehie <67@Alxschool.com>
-  */
+#include <stdio.h>
 
- #include <stdio.h>
- 
- /**
-  * main - Prints "Programming is like bulding a 
-  *                multilingual puzzle, followed by a new line.
-  *
-  * Return: Always 0.
-  */
- int main(void)
- { 
+/**
+ * main - Prints "Programming is like bulding a 
+ *                multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
- }
+}
