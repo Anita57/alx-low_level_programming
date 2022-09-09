6a559c1a24ec699507a8638100ea2740f0061931
Anita57 <anyaehieoluomachi@gmail.com>
2022-09-09 07:02:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main-Print exactly this words "programming is like bulding a 
- *                               multilingual puzzle, followed by a new line.
+ * main - Prints "programming is like bulding a 
+ *            multilingual puzzle, followed by a new line.
  *
  * Return: Always 0.
  */
