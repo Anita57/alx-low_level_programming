064bc4f9b00211cde412ecea75885f4e3603e975
Anita57 <anyaehieoluomachi@gmail.com>
2022-09-09 06:46:32 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*print exactly this words "programming is like bulding a multilingual puzzle"
+ * use the function puts
+ * don't use printf
+ * Your program should end in value zero
+ */
+int main(void)
+{
+	puts ("\"programming is like building a multilingual puzzle");
+
+	return (0);
+}
