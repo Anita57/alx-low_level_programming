09e0b9ba25c8e5080dc6afd8dc26a1e023539793
Anita57 <anyaehieoluomachi@gmail.com>
2022-09-12 16:00:53 -0700

    5-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome a piece of art," 
+ * main - Prints "with proper grammar, but the outcome a piece of art,"
  * Followed by a new line
- *          
+ *
  * Return: Always 0.
  */
 int main(void)
